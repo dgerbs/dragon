48a5f98b90c8e54e1ca0167f6bf86082cd50264c
dgerbs <dojoh@outlook.com>
2013-11-23 13:27:36 -0500

    initialize dragon

--- dragon.rb
+++ dragon.rb
@@ -1,3 +1,16 @@
+# Create a living dragon
+
+puts "\nHi there, this is my pet dragon.\n\n"
+
 class Dragon
 
+  def initialize name
+    @name = name
+    @asleep = false
+    @stomach = 10 # Full
+    @bladder = 0 # No need to go
+
+    puts "#{name} is born."
+  end
+  
 end
